7ce30cd8b14802d8a37bc07f9c11f7faa42d6e7f
Chihoko Ueno <harrierraum@hotmail.com>
2020-06-30 17:26:00 +0200

    create repo in heroku

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+before_action :set_cocktail, only: [:show]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @cocktails = Cocktail.where("name iLike '%#{params[:query]}%'")
+    else
+      @cocktails = Cocktail.all
+    end
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @dose = Dose.new
+    @ingredient = Ingredient.new
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
